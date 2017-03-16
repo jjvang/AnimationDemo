14431fda41d1d76a561bf01e7ea0b4a243537617
jjvang <jjv4ko@gmail.com>
2017-03-16 15:02:38 -0700

    finished manually adding text and images, need to next add an adapter or custom object to transfer an custom arraylist

--- app/src/main/java/com/example/ojboba/animationdemo/MainActivity.java
+++ app/src/main/java/com/example/ojboba/animationdemo/MainActivity.java
@@ -11,9 +11,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         viewPager=(ViewPager)findViewById(R.id.viewPager);
         customSwip=new customSwip(this);
         viewPager.setAdapter(customSwip);
+
+
+
     }
 }
--- app/src/main/java/com/example/ojboba/animationdemo/customSwip.java
+++ app/src/main/java/com/example/ojboba/animationdemo/customSwip.java
@@ -7,12 +7,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Torab on 20-May-16.
  */
 public class customSwip extends PagerAdapter {
     private int [] imageResources ={R.drawable.capture1,R.drawable.capture2,R.drawable.capture3,R.drawable.capture4,R.drawable.capture5};
+    private String[] imageText = {"one", "two", "three", "four", "five"};
+    private String[] imageHmong = {"61236", "612361", "612361", "612361", "612361"};
     private Context ctx;
     private LayoutInflater layoutInflater;
 
@@ -30,13 +33,22 @@ public class customSwip extends PagerAdapter {
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        layoutInflater= (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        layoutInflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View itemView=layoutInflater.inflate(R.layout.activity_custom_swip,container,false);
         ImageView imageView=(ImageView) itemView.findViewById(R.id.swip_image_view);
-        TextView textView=(TextView) itemView.findViewById(R.id.imageCount);
+        TextView textHmong=(TextView) itemView.findViewById(R.id.hmong);
+        TextView textEnglish=(TextView) itemView.findViewById(R.id.english);
         imageView.setImageResource(imageResources[position]);
-        textView.setText("Image Counter :"+position);
+        final String dummytext = imageText[position];
+        textHmong.setText(imageHmong[position]);
+        textEnglish.setText(imageText[position]);
         container.addView(itemView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ctx, "" + dummytext, Toast.LENGTH_SHORT).show();
+            }
+        });
         return itemView;
     }
 
