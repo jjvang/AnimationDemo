4f2ff3ce0628819592972e9c195202a76b90abbf
jjvang <jjv4ko@gmail.com>
2017-03-16 20:16:42 -0700

    Still need to figure out how to transfer data from main activity to pageradapter

--- app/src/main/java/com/example/ojboba/animationdemo/MainActivity.java
+++ app/src/main/java/com/example/ojboba/animationdemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.ojboba.animationdemo;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,13 +13,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
         viewPager=(ViewPager)findViewById(R.id.viewPager);
         customSwip=new customSwip(this);
         viewPager.setAdapter(customSwip);
 
-
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // SOMEHOW PASS THESE DATA TO customSWIP to set new values
+                int [] imageResources ={R.drawable.capture1,R.drawable.capture2,R.drawable.capture3,R.drawable.capture4,R.drawable.capture5};
+                String[] imageText = {"new", "two", "three", "four", "five"};
+                String[] imageHmong = {"new", "new", "new", "new", "new"};
+            }
+        });
 
     }
 }
